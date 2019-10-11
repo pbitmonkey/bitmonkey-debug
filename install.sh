2094d42b94f16f07a938ce109ef80d553108a560
Paolo Pa <52039398+Bitmonkeyp@users.noreply.github.com>
2019-10-11 16:52:00 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev
+sudo apt-get install software-properties-common
+sudo apt-get install libminiupnpc-dev
+sudo apt-get install libzmq3-dev
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+sudo apt-get install libqrencode-dev
+sudo apt-get install autoconf libtool 
+sudo apt-get install gawk
+sudo apt-get install g++ make
+sudo apt install libboost-all-dev
+sudo apt-get install libminiupnpc-dev
+sudo apt-get install libqt4-dev libprotobuf-dev protobuf-compiler
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools
+sudo apt-get install autotools-dev 
+sudo apt-get libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev
+sudo apt-get install libzmq3-dev
+sudo apt-get install libqrencode-dev
+sudo apt-get install doxygen
